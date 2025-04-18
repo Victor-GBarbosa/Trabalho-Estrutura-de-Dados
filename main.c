eba76f86c2ae0c1354c3b89647e66ad1346c84d1
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-04-18 14:40:44 -0300

    Adicionado o Switch case com placeholders

--- main.c
+++ main.c
@@ -18,10 +18,45 @@ int main() {
         printf("Arquivo encontrado. Pronto para manipulação\n");
     }
 
-    //Arquivo aberto ou criado 
+    //Arquivo aberto ou Criado
 
+    int index, matricula;
+    char nome[50], materia[50]; 
+
+    //Estrutura CSV ==> index,nome,materia,matricula (Ex 0,Eduardo,Biologia,252009) 
     
-    
+    int opt;
+    do {
+        printf("\nMenu:\n");
+        printf("1- Ver matriculas\n");
+        printf("2- Adicionar matricula\n");
+        printf("3- Ornizar itens\n");
+        printf("4. Remover matricula\n");
+        printf("5. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opt);
+
+        switch (opt) {
+            case 1:
+                // Código para Ver alunos matriulados
+                break;
+            case 2:
+                // Código para Adicionar uma nova matricula
+                break;
+            case 3:
+                // Código para reorganizar as matriculas
+                break;
+            case 4:
+                // Código para remover uma matricula
+                break;
+            case 5:
+                printf("Saindo do programa...\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opt != 5);
+
     //Finalizando o programa
 
     fclose(arquivo);
