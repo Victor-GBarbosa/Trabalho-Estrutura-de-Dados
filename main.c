2c28811960e712b6d76b1a5960a4e665faa1acce
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-04-18 15:25:53 -0300

    Criação-da-matricula
    Codigo de criação da matricula esta concluido, ainda falta adiciona-la ao arquivo

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main() {
 
-    const char *fname = "Arquivo.txt";
+    srand(time(NULL)); //Gerando a seed para o srand baseado no tempo no sistema operacional
 
-    FILE *arquivo = fopen(fname, "r+");
+    const char *mainFName = "Arquivo.txt";
 
-    if (arquivo == NULL) {
+    FILE *arquivo = fopen(mainFName, "r+"); //Tentando abrir o arquivo
+
+    if (arquivo == NULL) { //Verificando se o arquivo foi aberto
         printf("Criando Arquivo.\n");
-        arquivo = fopen(fname, "w");
+        arquivo = fopen(mainFName, "w"); //Craiando um arquivo caso não seja encontrado
         if (arquivo == NULL) {
             printf("Erro ao criar o arquivo");
             return 1;
@@ -18,15 +23,17 @@ int main() {
         printf("Arquivo encontrado. Pronto para manipulação\n");
     }
 
+    fclose(arquivo); // Fechando o arquivo para que possa ser aberto de diferentes formas futuramente
+
     //Arquivo aberto ou Criado
 
-    int index, matricula;
-    char nome[50], materia[50]; 
+    int index;
+    char nome[50], materia[50], matricula[6]; // Increased size to accommodate null terminator
 
-    //Estrutura CSV ==> index,nome,materia,matricula (Ex 0,Eduardo,Biologia,252009) 
+    //Estrutura CSV ==> index,nome,materia,matricula (Ex 0,Eduardo,Biologia,25209) 
     
     int opt;
-    do {
+    do { //iniciando o menu do programa
         printf("\nMenu:\n");
         printf("1- Ver matriculas\n");
         printf("2- Adicionar matricula\n");
@@ -35,13 +42,23 @@ int main() {
         printf("5. Sair\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opt);
+        int i;
+        
 
         switch (opt) {
             case 1:
                 // Código para Ver alunos matriulados
                 break;
             case 2:
-                // Código para Adicionar uma nova matricula
+              printf("Digite o nome do aluno");
+              scanf("%s", nome);
+              printf("Digite a materia em que o aluno sera matriculado");
+              scanf("%s", materia);
+              for (i = 0; i < 5; i++) { //Criando a matricula
+                matricula[i] = '0' + (rand() % 10); // Convertendo int's para strings
+            }
+                matricula[5] = '\0'; // Fechando a string
+              
                 break;
             case 3:
                 // Código para reorganizar as matriculas
